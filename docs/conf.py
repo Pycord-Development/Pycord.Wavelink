c1ffa637031f1dd502db96646ddb3b976418d91c
Lala Sabathil <lala@pycord.dev>
2022-01-09 10:00:04 +0100

    Fix docs eye burn (#21)

--- docs/conf.py
+++ docs/conf.py
@@ -1,77 +1,78 @@
-# pycord.wavelink documentation master file, created by
-# sphinx-quickstart on Tue Dec 14 13:13:44 2021.
-# You can adapt this file completely to your liking, but it should at least
-# contain the root `toctree` directive.
-# ----------------------------------------------------------------------------
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
 import os
 import sys
+import re
+
 sys.path.insert(0, os.path.abspath("../src"))
+sys.path.append(os.path.abspath('extensions'))
 
-
-# -- Project information -----------------------------------------------------
-
-project = "pycord.wavelink"
+project = "Pycord.Wavelink"
 copyright = "2021, Pycord Development"
 author = "Pycord Development"
 
-# The full version, including alpha/beta/rc tags
-release = "1.0.0"
+version = ''
+with open('../src/pycord/wavelink/__init__.py') as f:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
 
+release = version
 
-# -- General configuration ---------------------------------------------------
+branch = 'main' if version.endswith('a') else 'v' + version
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
+extlinks = {
+    'issue': ('https://github.com/Pycord-Development/Pycord.Wavelink/issues/%s', 'GH-'),
+}
+
+intersphinx_mapping = {
+  'py': ('https://docs.python.org/3', None),
+  'aio': ('https://docs.aiohttp.org/en/stable/', None),
+  'req': ('https://docs.python-requests.org/en/latest/', None),
+  'discord': ('https://docs.pycord.dev/en/master/', None)
+}
+
 extensions = [
-    "sphinx.ext.autodoc",
-    "sphinx.ext.intersphinx",
-    "sphinx.ext.todo",
+    'builder',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.extlinks',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.napoleon',
+    'sphinxcontrib_trio',
+    'details',
+    'exception_hierarchy',
+    'attributetable',
+    'resourcelinks',
+    'nitpick_file_ignorer',
 ]
 
-# Add any paths that contain templates here, relative to this directory.
+autodoc_member_order = 'bysource'
+autodoc_typehints = 'none'
+
 templates_path = ["_templates"]
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = "furo"
+html_theme = "basic"
 html_logo = "./assets/pycord.png"
-html_theme_options = {
-    "light_css_variables": {
-        "color-brand-primary": "#4C8CBF",
-        "color-brand-content": "#306998",
-        "color-admonition-background": "blue",
-    },
-    "dark_css_variables": {
-        "color-brand-primary": "#306998",
-        "color-brand-content": "#FFE871",
-        "color-admonition-background": "yellow",
-    },
-    "sidebar_hide_name": True,
+html_experimental_html5_writer = True
+html_context = {
+  'discord_invite': 'https://pycord.dev/discord',
+  'pycord_wavelink_extensions': [
+  ('pycord.wavelink.ext.spotify', 'ext/spotify'),
+],
 }
-pygments_style = "monokai"
+
+resource_links = {
+  'discord': 'https://pycord.dev/discord',
+  'issues': 'https://github.com/Pycord-Development/Pycord.Wavelink/issues',
+  'discussions': 'https://github.com/Pycord-Development/Pycord.Wavelink/discussions',
+  'examples': f'https://github.com/Pycord-Development/Pycord.Wavelink/tree/{branch}/examples',
+}
 default_dark_mode = True
+
+gettext_compact = False
+
+language = None
+
+source_suffix = '.rst'
+master_doc = 'index'
 
 rst_prolog = """
 .. |coro| replace:: This function is a |coroutine_link|_.\n\n
@@ -82,22 +83,31 @@ rst_prolog = """
     <div class="default-value-section"> <span class="default-value-label">Default:</span>
 """
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 
+html_search_scorer = '_static/scorer.js'
 
-# -- Extension configuration -------------------------------------------------
+html_js_files = [
+  'custom.js',
+  'settings.js',
+  'copy.js',
+  'sidebar.js'
+]
 
-# -- Options for intersphinx extension ---------------------------------------
+htmlhelp_basename = 'pycordwavedoc'
+html_favicon = './assets/pycord.ico'
+latex_documents = [
+  ('index', 'pycord.wavelink.tex', 'Pycord.Wavelink Documentation',
+   'Pycord Development', 'manual'),
+]
 
-# Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {
-    "python": ("https://docs.python.org/3", None),
-}
+man_pages = [
+    ('index', 'pycord.wavelink', 'Pycord.Wavelink Documentation',
+     ['Pycord Development'], 1)
+]
 
-# -- Options for todo extension ----------------------------------------------
-
-# If true, `todo` and `todoList` produce output, else they produce nothing.
-todo_include_todos = True
+texinfo_documents = [
+  ('index', 'pycord.wavelink', 'Pycord.Wavelink Documentation',
+   'Pycord Development', 'pycord.wavelink', 'One line description of project.',
+   'Miscellaneous'),
+]
--- setup.py
+++ setup.py
@@ -22,17 +22,61 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 DEALINGS IN THE SOFTWARE.
 """
+import re
 
 from setuptools import setup
 
 with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
-version = "{{__VERSION__}}"
+version = ""
+with open("src/pycord/wavelink/__init__.py") as f:
+
+    search = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE)
+
+    if search is not None:
+        version = search.group(1)
+
+    else:
+        raise RuntimeError("Could not grab version string")
+
+if not version:
+    raise RuntimeError("version is not set")
+
+if version.endswith(("a", "b", "rc")):
+    # append version identifier based on commit count
+    try:
+        import subprocess
+
+        p = subprocess.Popen(["git", "rev-list", "--count", "HEAD"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = p.communicate()
+        if out:
+            version += out.decode("utf-8").strip()
+        p = subprocess.Popen(["git", "rev-parse", "--short", "HEAD"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = p.communicate()
+        if out:
+            version += "+g" + out.decode("utf-8").strip()
+    except Exception:
+        pass
 
 readme = ""
 with open("README.rst") as f:
     readme = f.read()
+
+extras_require = {
+    "voice": ["PyNaCl>=1.3.0,<1.6"],
+    "docs": [
+        "sphinx==4.3.2",
+        "sphinxcontrib_trio==1.1.2",
+        "sphinxcontrib-websupport",
+    ],
+    "speed": [
+        "orjson>=3.5.4",
+        "aiodns>=1.1",
+        "Brotlipy",
+        "cchardet",
+    ],
+}
 
 setup(
     name="pycord.wavelink",
@@ -44,6 +88,7 @@ setup(
     license="MIT",
     description="A robust and powerful Lavalink wrapper for pycord.",
     long_description=readme,
+    extras_require=extras_require,
     include_package_data=True,
     install_requires=requirements,
     classifiers=[
